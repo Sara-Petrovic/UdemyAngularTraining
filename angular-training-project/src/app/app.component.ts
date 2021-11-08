742e1665224f11772446be14b30b10e58ef7ab53
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-11-08 09:19:07 +0100

    Izmenjena App komponenta

--- angular-training-project/src/app/app.component.ts
+++ angular-training-project/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-training-project';
+  title = 'angular-training-project is running!';
+
+  firstName = 'Sara';
+  lastName = 'Petrovic';
 }
