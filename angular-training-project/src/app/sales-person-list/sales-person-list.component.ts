6fe2db8b83675a244ca0651f1ef50604a04cde92
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-11-08 10:25:22 +0100

    Kreirana lista SalesPerson-a

--- angular-training-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+
+  //kreiramo listu objekata
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Sara', 'Petrovic', 'sara@gmail.com',10000),
+    new SalesPerson('Marko', 'Lucic', 'marko@gmail.com',40000),
+    new SalesPerson('Dragan', 'Petrovic', 'dragan@gmail.com',90000),
+    new SalesPerson('Pera', 'Peric', 'pera@gmail.com',60000)
+
+  ];
 
   constructor() { }
 
